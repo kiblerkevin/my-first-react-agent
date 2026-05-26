32c1d2a2eb6dba7db76bc93622fd76a4054657c5
Kevin <kevin@Kevins-MacBook-Pro.local>
2026-05-26 10:50:44 -0500

    update s3 config

--- infra/modules/s3-spa/main.tf
+++ infra/modules/s3-spa/main.tf
@@ -18,3 +18,13 @@ resource "aws_s3_bucket_versioning" "spa" {
     status = "Enabled"
   }
 }
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "spa" {
+  bucket = aws_s3_bucket.spa.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
